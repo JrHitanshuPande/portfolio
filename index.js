f268149913edb99e0af7bdc114092cf61a8e3d2b
JrHitanshuPande <hitanshupande2021@gmail.com>
2023-03-11 21:41:13 +0530

    Mobile nav

--- index.js
+++ index.js
@@ -24,6 +24,33 @@ const cont = document.querySelector(".contactme");
 document.querySelector(".contacts").addEventListener("click", () => {
     educa.scrollIntoView({ behavior: "smooth"});
 });
+
+// // Mobile Smooth Scroll 
+const about1 = document.querySelector(".about0");
+
+document.querySelector(".about2").addEventListener("click", () => {
+    about.scrollIntoView({ behavior: "smooth" });
+});
+
+const skillz1 = document.querySelector(".skills");
+
+document.querySelector(".skill1").addEventListener("click", () => {
+    skillz.scrollIntoView({ behavior: "smooth" });
+});
+
+
+
+const educa1 = document.querySelector(".Education");
+
+document.querySelector(".Educat1").addEventListener("click", () => {
+    educa.scrollIntoView({ behavior: "smooth"});
+});
+
+const cont1 = document.querySelector(".contactme");
+
+document.querySelector(".contacts1").addEventListener("click", () => {
+    educa.scrollIntoView({ behavior: "smooth"});
+});
 // Gamil hidden 
 const viewButton = document.querySelector(".button");
 const email = document.querySelector(".hidden");
@@ -37,3 +64,15 @@ viewButton.addEventListener("click", () => {
       viewButton.textContent="View";
     }
   });
+
+
+  // Mobile Nav Bar
+
+  const navbut = document.querySelector(".fa-bars") ;
+  navbut.onclick = function ()
+  {
+    
+    mobilenavb = document.querySelector(".mobilenav");
+    mobilenavb.classList.toggle("active");
+  }
+  
